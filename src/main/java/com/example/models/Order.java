8c1c938c49ab2f6332e08eed88010bd056fe09d1
Nghia Truong <tmnghia1992@gmail.com>
2021-11-15 17:01:59 +0700

    update bill format

--- src/main/java/com/example/models/Order.java
+++ src/main/java/com/example/models/Order.java
@@ -115,6 +115,8 @@ public class Order implements Serializable {
             str.append(",");
             str.append(quantity*itemPrice);
         }
+        str.append("\n,,,");
+        str.append(getTotalPrice());
 
         return str.toString();
     }
